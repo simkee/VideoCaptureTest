3dcf629a3208012d6cc6ec3884ce12088dd0c692
Simon <simon@Netmont.home>
2015-05-28 00:10:24 +0200

    format correctly cpp file

--- src/videocap.cpp
+++ src/videocap.cpp
@@ -1,7 +1,7 @@
 #include "opencv2/opencv.hpp"
-	
+
 #include <stdio.h>
-	
+
 
 #include <iostream>
 
@@ -14,23 +14,22 @@ int main(int, char**)
     if(!cap.isOpened())  // check if we succeeded
         return -1;
 
-	cout << "fps" << cap.get(CV_CAP_PROP_FPS) << endl;
+    cout << "fps" << cap.get(CV_CAP_PROP_FPS) << endl;
 
-	VideoWriter vidOut;
-	Mat vidOutFrame;
-    
-    	vidOutFrame.create(480, 640, CV_8UC3);
+    VideoWriter vidOut;
+    Mat vidOutFrame;
 
-	vidOut.open("starter_video_out.avi", CV_FOURCC('X', 'V', 'I', 'D'), 25, 
-				vidOutFrame.size(), true);
+    vidOutFrame.create(480, 640, CV_8UC3);
 
-cout << "create file	" << endl;
-	    
+    vidOut.open("starter_video_out.avi", CV_FOURCC('X', 'V', 'I', 'D'), 25,
+                vidOutFrame.size(), true);
+
+    cout << "create file	" << endl;
+
     Mat edges;
     namedWindow("edges",1);
     int n=0;
-    for(;;)
-    {
+    for(;;) {
         Mat frame;
         cap >> frame; // get a new frame from camera
         //~ cvtColor(frame, edges, CV_BGR2GRAY);
@@ -38,22 +37,22 @@ cout << "create file	" << endl;
         //~ Canny(edges, edges, 0, 30, 3);
         imshow("edges", frame);
 
-	frame.copyTo(vidOutFrame);
-	vidOut << vidOutFrame;
-	//~ vidOut << frame;
-	    	cout << "fps" << cap.get(CV_CAP_PROP_FPS) << endl;
-	    	cout << "fp counts" << cap.get(CV_CAP_PROP_FRAME_COUNT) << endl;
+        frame.copyTo(vidOutFrame);
+        vidOut << vidOutFrame;
+        //~ vidOut << frame;
+        cout << "fps" << cap.get(CV_CAP_PROP_FPS) << endl;
+        cout << "fp counts" << cap.get(CV_CAP_PROP_FRAME_COUNT) << endl;
 
-	if(waitKey(30) >= 0) break;
+        if(waitKey(30) >= 0) break;
 
-	//~ char filename[200];
-	//~ sprintf(filename,"filename%.3d.jpg",n++);
-	//~ imwrite(filename,frame)
-	//~ imwrite(filename,frame)
-	//~ cout << "Saved " << filename << endl;
-	    
-	    
+        //~ char filename[200];
+        //~ sprintf(filename,"filename%.3d.jpg",n++);
+        //~ imwrite(filename,frame)
+        //~ imwrite(filename,frame)
+        //~ cout << "Saved " << filename << endl;
+
+
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
-}	
+}
